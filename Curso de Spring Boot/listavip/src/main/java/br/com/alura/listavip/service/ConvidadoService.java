713b6aa821c27efb16443701e04515bc80774e02
Guilherme Santos Padilha <guilherme.padilha@poa01.local>
2019-12-24 12:31:53 -0300

    Curso de  Spring boot

---
+++ Curso de Spring Boot/listavip/src/main/java/br/com/alura/listavip/service/ConvidadoService.java
@@ -0,0 +1,26 @@
+package br.com.alura.listavip.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.alura.listavip.model.Convidado;
+import br.com.alura.listavip.repository.ConvidadoRepository;
+
+@Service
+public class ConvidadoService {
+	
+	@Autowired
+	private ConvidadoRepository repository;
+	
+	public Iterable<Convidado> obterTodos(){
+		return repository.findAll();
+	}
+	
+	public void salvar (Convidado convidado) {
+		repository.save(convidado);
+	}
+
+	public void apagar(Long id) {
+		repository.deleteById(id);
+	}
+}
